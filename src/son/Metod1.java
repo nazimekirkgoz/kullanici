726eef287076339fee0eeefa8d3f3d0bd7575866
Nazime <kirkgoznazime@gmail.com>
2023-01-03 06:38:41 +0100

    dosya

---
+++ src/son/Metod1.java
@@ -0,0 +1,11 @@
+package son;
+
+public class Metod1 {
+    public static void main(String[] args) {
+        String cumle="bugun hava cok guzel";
+        System.out.println(cumle);
+
+    }
+
+
+}
