0041341153f766e84b66a745925bc2378a304230
Nazime <kirkgoznazime@gmail.com>
2023-01-04 12:44:42 +0100

    29.gun

---
+++ src/day29_final_AbstrctClasses/GAbstractChild.java
@@ -0,0 +1,15 @@
+package day29_final_AbstrctClasses;
+
+public abstract class GAbstractChild extends Dparent {
+
+
+    public  void  method1(){
+
+    }
+    public  abstract void method4();
+
+    public  void method5(){
+
+    }
+
+}
